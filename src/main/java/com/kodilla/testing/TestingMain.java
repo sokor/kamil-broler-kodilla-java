914f27b1be6f246dc4780b89ab742c0691d43bb4
sokor@o2.pl <sokor@o2.pl>
2018-07-01 20:34:22 +0200

    module 6.2

--- src/main/java/com/kodilla/testing/TestingMain.java
+++ src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,40 @@
 package com.kodilla.testing;
 
-public class TestingMain {
-    public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
-    }
-}
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
+ public class TestingMain {
+        public static void main(String[] args){
+            SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+            String result = simpleUser.getUsername();
+
+            if (result.equals("theForumUser")){
+                System.out.println("test OK");
+            } else {
+                System.out.println("Error!");
+            }
+            System.out.println("Test - pierwszy test jednostkowy:");
+
+            Calculator calculator = new Calculator();
+            int addResult = calculator.add(5,8);
+
+            if (addResult>=5){
+                System.out.println("addition test - OK");
+            }
+            else{
+                System.out.println("addition test - Error!");
+            }
+
+            int subtructResult = calculator.subtruct(8,12);
+
+            if (subtructResult<8){
+                System.out.print("subtruct test - OK");
+            }
+            else{
+                System.out.println("subtruct test - Error!");
+            }
+
+
+        }
+ }
